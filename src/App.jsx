fa7c1a5c26cbe27b39d50d5925873a5dbda61756
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-22 13:45:19 +0530

    src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Video_Room from './Video_Room';
+import ZegoCloud from './ZegoCloud';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ZegoCloud />} />
+        <Route path="/room/:id" element={<Video_Room />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
